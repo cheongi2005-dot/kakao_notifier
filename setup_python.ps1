9d6eba12ca286d127292db8a2e54c1c321f8aa1f
cheongi2005-dot <cheongi2005@gmail.com>
2026-05-27 17:48:54 +0900

    Use WebClient for faster Python download instead of Invoke-WebRequest

--- setup_python.ps1
+++ setup_python.ps1
@@ -3,8 +3,10 @@ $ErrorActionPreference = "Stop"
 $tmp = $env:TEMP
 
 Write-Host "[1/2] Downloading Python 3.13..."
-Invoke-WebRequest "https://www.python.org/ftp/python/3.13.3/python-3.13.3-amd64.exe" `
-    -OutFile "$tmp\python-installer.exe" -UseBasicParsing
+(New-Object System.Net.WebClient).DownloadFile(
+    "https://www.python.org/ftp/python/3.13.3/python-3.13.3-amd64.exe",
+    "$tmp\python-installer.exe"
+)
 
 Write-Host "[2/2] Installing Python..."
 $p = Start-Process -FilePath "$tmp\python-installer.exe" `
